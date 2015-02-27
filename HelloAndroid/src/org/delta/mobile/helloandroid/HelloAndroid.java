be2fc666cba64e0249963dee390360f2a26b4041
ca31085 <ca31085@CAL1102440.deltads.ent>
2015-02-27 01:10:48 -0800

    xzz

---
+++ HelloAndroid/src/org/delta/mobile/helloandroid/HelloAndroid.java
@@ -0,0 +1,30 @@
+package org.delta.mobile.helloandroid;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.AnalogClock;
+import android.widget.TextView;
+
+//Hello
+public class HelloAndroid extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+    	super.onCreate(savedInstanceState);
+        this.getAnalogClockView();
+        
+    }
+        
+    public void getAnalogClockView() {
+    	AnalogClock tv = new AnalogClock(this);
+        tv.setActivated(true);
+        tv.setBackgroundColor(BIND_AUTO_CREATE);
+        setContentView(tv);
+    }
+    
+    public void getTextView() {
+    	TextView tv = new TextView(this);
+        tv.setText("Hello Sen, Android");
+        setContentView(tv);
+    }
+}
